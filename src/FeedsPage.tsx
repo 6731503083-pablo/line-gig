e82b9017e7cfe02bdc6c10ec11006a24a006ce94
Phyo Wai Yan Lin Zaw <6731503083@lamduan.mfu.ac.th>
2025-08-02 11:26:30 +0000

    feeds page test

--- src/FeedsPage.tsx
+++ src/FeedsPage.tsx
@@ -1,3 +1,17 @@
+import liff from "@line/liff";
+import { useLocation, useNavigate } from "react-router-dom";
+
 export const FeedsPage = () => {
-  return <div>FeedsPage</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const type = location.state?.type as "employer" | "freelancer" | undefined;
+  return <div>
+    <div>FeedsPage</div>
+    <div>{type}</div>
+    <div onClick={()=>{
+      liff.logout();
+      localStorage.removeItem("userType");
+      navigate("/");
+    }}>Log Out</div>
+  </div>;
 };
